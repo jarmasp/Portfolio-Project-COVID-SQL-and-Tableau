96c34e150c8b50eb3c78fa8bdda9503a0750c318
thyfus <44787238+thyfus@users.noreply.github.com>
2022-04-13 09:41:02 -0400

    SQL queries used to data for the tableau visualization

---
+++ SQLQuery for covid tablue project.sql
@@ -0,0 +1,28 @@
+-- queries used to obtain the data to display on tableau
+
+select sum(new_cases) as total_cases, sum(cast(new_deaths as int)) as total_deaths, sum(cast(new_deaths as int))/sum(new_cases)*100 as death_percentage
+from PortfolioProject..CovidDeaths
+where continent is not null 
+order by 1, 2 
+
+-- We take these out as they are not inluded in the above queries and want to stay consistent
+-- European Union is part of Europe, also retiring Upper middle income, High income, Lower middle income and Low income
+
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+Where continent is null 
+and location not in ('World', 'European Union', 'International', 'Upper middle income', 'High income', 'Lower middle income', 'Low income')
+Group by location
+order by TotalDeathCount desc
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+Group by Location, Population, date
+order by PercentPopulationInfected desc
